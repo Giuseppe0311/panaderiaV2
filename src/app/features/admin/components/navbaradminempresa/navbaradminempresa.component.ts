b3b8c7de85a0f194c8f1a1b95f7b501cb46e52a3
Giuseppe0311 <barrerita_romero@hotmail.com>
2024-02-16 23:13:07 -0500

    SE AÑADIO CAMBIOS

--- src/app/features/admin/components/navbaradminempresa/navbaradminempresa.component.ts
+++ src/app/features/admin/components/navbaradminempresa/navbaradminempresa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../../auth/services/auth.service';
+import { ApiServiceService, ApiType } from '../../../../core/services/api-service.service';
 
 @Component({
   selector: 'app-navbaradminempresa',
@@ -14,7 +15,20 @@ export class NavbaradminempresaComponent implements OnInit {
   isLogged = false;
   router = inject(Router);
   route = inject(ActivatedRoute);
+  apiservice = inject(ApiServiceService)
+  data : any ={}
   ngOnInit(): void {
+    this.route.url.subscribe(() => {
+      // Aquí, en lugar de suscribirse a 'url', que solo te dará los segmentos de la URL del componente actual,
+      // podrías querer suscribirte a 'params' o 'paramMap' para obtener los parámetros de la ruta.
+      this.route.paramMap.subscribe(params => {
+        let idempresa = Number(params.get('idempresa')); // Obtiene el valor de 'idempresa'
+        this.apiservice.getData(ApiType.Public,'empresas',{"idempresa":idempresa}).subscribe(data=>{
+          console.log(data)
+          this.data= data
+        });
+      });
+    });
     this.authservice.currentUserLoginOn.subscribe(data => {
       if(data){
         console.log(data)
--- src/app/features/sucursalAdmin/components/navbarsucursal-admin/navbarsucursal-admin.component.ts
+++ src/app/features/sucursalAdmin/components/navbarsucursal-admin/navbarsucursal-admin.component.ts
@@ -2,6 +2,7 @@ import { NgClass } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../../auth/services/auth.service';
+import { ApiServiceService, ApiType } from '../../../../core/services/api-service.service';
 
 @Component({
   selector: 'app-navbarsucursal-admin',
@@ -18,7 +19,28 @@ export class NavbarsucursalAdminComponent implements OnInit {
   router = inject(Router);
   route = inject(ActivatedRoute);
   private authservice = inject(AuthService);
+  private apiservice = inject(ApiServiceService)
+  logo :string = ''
+  nombreEmpresa: string = ''
+  nombreSucursal: string = ''
   ngOnInit(): void {
+    this.route.url.subscribe(() => {
+      // Aquí, en lugar de suscribirse a 'url', que solo te dará los segmentos de la URL del componente actual,
+      // podrías querer suscribirte a 'params' o 'paramMap' para obtener los parámetros de la ruta.
+      this.route.paramMap.subscribe(params => {
+        let idempresa = Number(params.get('idempresa')); // Obtiene el valor de 'idempresa'
+        let idsucursal = Number(params.get('idsucursal')); // Obtiene el valor de 'idsucursal'
+        this.apiservice.getData(ApiType.Public,'empresas',{"idempresa":idempresa}).subscribe(data=>{
+          console.log(data)
+          this.logo= data.logo
+          this.nombreEmpresa = data.nombre
+        });
+        this.apiservice.getData(ApiType.Public,'sucursales',{"idsucursal":idsucursal}).subscribe(data=>{
+          console.log(data)
+          this.nombreSucursal = data.nombre
+        });
+      });
+    });
     this.authservice.currentUserLoginOn.subscribe(data => {
       if(data){
         console.log(data)
